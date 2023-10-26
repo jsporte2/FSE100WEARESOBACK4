5c4408ee88f92f445a8dbfd7d5d0a6ee7e8b5ce6
jsporte2 <148173648+jsporte2@users.noreply.github.com>
2023-10-25 19:21:34 -0700

    Update p5.js

--- p5.js
+++ p5.js
@@ -1,1 +1,20 @@
 
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220); 
+  
+  circle(50, 70, 50) // cirlce 1 
+  
+  circle(200, 70, 50) // cirlce 2 
+  circle(350, 70, 50) // cirlce 3 
+  circle(130, 150, 50) // cirlce 4  
+  circle(280, 150, 50) // cirlce 5 
+  circle(50, 250, 50) // cirlce 6
+  circle(200, 250, 50) // cirlce 7
+  circle(350, 250, 50) // cirlce 8 
+  circle(280, 350, 50) // cirlce 9
+  circle(130, 350, 50) // cirlce 10
+}
